0cec791f2116cd525b7598171d403fbc77d876df
jessicacliong <14395@coderacademy.edu.au>
2023-08-22 20:14:48 +1000

    feat nmp project created, basic script implemented

---
+++ index.js
@@ -0,0 +1,11 @@
+let {messagesToShare} = require('./messageList');
+
+function helloWorld(someMessageToSay){
+	console.log(someMessageToSay);
+}
+
+helloWorld("hello there!");
+
+for (const message of messagesToShare) {
+	helloWorld(message);
+}
